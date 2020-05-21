61de8b55f0275a820b6eb456e8e50bb75b66e35e
Ramish Fuh Shu <Agamatimtim@gmail.com>
2020-05-21 19:57:44 +0300

    Created_core_types

---
+++ Models/Interfaces/IEvents.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.Interfaces
+{
+    interface IEvents
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Interfaces/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Interfaces
+{
+    interface IStudent
+    {
+        public int ID { get; set; }
+        [Required]
+        public string  Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required][DataType(DataType.Date)]
+        public DateTime Admitted { get; set; }
+        [Required]
+        public bool TuitionPaid { get; set; }
+    }
+}
