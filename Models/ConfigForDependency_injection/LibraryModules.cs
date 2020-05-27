780c77db5c8ef11f243c3367a10a02699e778656
Ramish Fuh Shu <Agamatimtim@gmail.com>
2020-05-27 20:00:37 +0300

    added webapi endpoint

---
+++ Models/ConfigForDependency_injection/LibraryModules.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using Models.Core.Interfaces;
+using Models.Core.UnitOfWork;
+using Models.DataAccess.DataContext;
+using Models.Entities;
+using Models.EntityInterfaces;
+
+namespace Models.ConfigForDependency_injection
+{
+   public class LibraryModules : Module
+    {  /// <summary>
+    ///  registers all entities and repositories as interfaces
+    /// </summary>
+    /// <param name="builder"> autofac containerBuilder</param>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(ThisAssembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces();
+
+            // register Context
+            builder.RegisterType<SchoolAppContext>().AsSelf();
+            // register all classes here
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+            builder.RegisterType<Accomodation>().As<IAccomodation>();
+            builder.RegisterType<Admin>().As<IAdmin>();
+            builder.RegisterType<Class>().As<IClass>();
+            builder.RegisterType<ClubAndSociety>().As<IClubAndSociety>();
+            builder.RegisterType<Course>().As<ICourse>();
+            builder.RegisterType<CreditCard>().As<ICreditCard>();
+            builder.RegisterType<Event>().As<Event>();
+            builder.RegisterType<Grade>().As<IGrade>();
+            builder.RegisterType<Room>().As<IRoom>();
+            builder.RegisterType<Student>().As<IStudent>();
+            builder.RegisterType<Subscription>().As<ISubscription>();
+            builder.RegisterType<Teacher>().As<ITeacher>();
+        }
+       
+    }
+}
